afc2e195f2fe77192d4c576a5e6afd2cd2beb7d2
Carlos Rodriguez <carlos@s8f.org>
2012-07-21 16:57:15 -0700

    use better test path

--- test/basic.js
+++ test/basic.js
@@ -21,7 +21,7 @@ describe('basic test', function() {
     }).listen(port, done);
   });
   it('performs a request', function(done) {
-    request({url: baseUrl + '/', json: true}, function(err, res, data) {
+    request({url: baseUrl + '/test/url', json: true}, function(err, res, data) {
       assert.equal(res.statusCode, 200);
       assert.strictEqual(data.hello, 'world');
       done();
@@ -29,7 +29,7 @@ describe('basic test', function() {
   });
   it('can tail the log', function(done) {
     exec('tail ' + tmpLog, function(err, stdout, stderr) {
-      assert(/^127\.0\.0\.1 \- \- \[[0-9]{2}\/\w{3}\/[0-9]{4}:[0-9]{2}:[0-9]{2}:[0-9]{2} (\+|\-)[0-9]{4}\] "GET \/ HTTP\/1\.1" 200 17 "\-" "\-"\n$/.exec(stdout));
+      assert(/^127\.0\.0\.1 \- \- \[[0-9]{2}\/\w{3}\/[0-9]{4}:[0-9]{2}:[0-9]{2}:[0-9]{2} (\+|\-)[0-9]{4}\] "GET \/test\/url HTTP\/1\.1" 200 17 "\-" "\-"\n$/.exec(stdout));
       done();
     });
   });
