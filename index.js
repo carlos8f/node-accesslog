48d953860fe5a3481d4c517d674b31d723d32882
Carlos Rodriguez <carlos@s8f.org>
2012-07-21 16:59:12 -0700

    Strip out \r better

--- index.js
+++ index.js
@@ -92,12 +92,12 @@ function zerofill(num, length) {
 function getHeader(res, name) {
   if (res._header && res._headerSent && !res._sentHeaders) {
     res._sentHeaders = {};
-    res._header.split('\n').forEach(function(line) {
+    res._header.split('\r\n').forEach(function(line) {
       var header = line.split(': ');
       if (header.length > 1) {
         res._sentHeaders[header[0]] = header[1];
       }
     });
   }
-  return (res._sentHeaders && res._sentHeaders[name].replace(/\r/g, '')) || null;
+  return (res._sentHeaders && res._sentHeaders[name]) || null;
 }
